8d6e696f2f0c0d8199e8903d8c755b1ddcc6c50d
Minindu Hewawasam <minindu@intervest.lk>
2022-06-23 05:58:25 +0530

    stack implementation

---
+++ stacks/Stack.java
@@ -0,0 +1,45 @@
+package stacks;
+
+public class Stack {
+    private int arr[];
+    private int top;
+    private int capacity;
+
+    public Stack(int size) {
+        this.arr = new int[size];
+        this.capacity = size;
+        this.top = -1;
+    }
+
+    public void push(int x) {
+        if (isFull()) {
+            System.out.println("Overflow\n Program Terminated");
+            return;
+        }
+
+        System.out.println("Inserting " + x);
+        arr[++top] = x;
+    }
+
+    public void pop() {
+        if (isEmpty()) {
+            System.out.println("stack is empty");
+            return;
+        }
+        top--;
+    }
+
+    public Boolean isEmpty() {
+        return top == -1;
+    }
+
+    public Boolean isFull() {
+        return top == capacity - 1;
+    }
+
+    public void printStack() {
+        for (int i = 0; i <= top; i++) {
+            System.out.println(arr[i]);
+        }
+    }
+}
